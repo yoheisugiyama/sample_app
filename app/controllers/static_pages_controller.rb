75413451ce8228f32e0f283f808444548d3abaa1
vagrant <vagrant@localhost.localdomain>
2016-01-02 05:28:08 +0000

    Finish layout and routes

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,11 +3,13 @@ class StaticPagesController < ApplicationController
   def home
   end
 
-
   def help
   end
 
   def about
   end
 
+  def contact
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,18 @@
 Rails.application.routes.draw do
 
+  get 'users/new'
+
   root 'static_pages#home'
-  get 'static_pages/help'
-  get 'static_pages/about'
+  get 'help'    => 'static_pages#help'
+  get 'about'   => 'static_pages#about'
+  get 'contact' => 'static_pages#contact'
+  get 'signup'  => 'users#new'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
